c66835af175933554f1c88a21d11042fb322316c
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-08 21:28:20 -0500

    init

---
+++ Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Control.Monad (forever)
+import System.Exit (exitSuccess)
+import Day1 (day1Main)
+import Day2 (day2Main)
+import Day3 (day3Main)
+import Day4 (day4Main)
+import Day5 (day5Main)
+import Day6 (day6Main)
+import Day7 (day7Main)
+import Day8 (day8Main)
+
+main :: IO ()
+main = forever $ do
+  putStrLn "\n\nWhat day? Enter 'q' to exit."
+  whichDay <- getLine
+  case whichDay of
+    "1" -> day1Main
+    "2" -> day2Main
+    "3" -> day3Main
+    "4" -> day4Main
+    "5" -> day5Main
+    "6" -> day6Main
+    "7" -> day7Main
+    "8" -> day8Main
+    "q" -> exitSuccess
+    _ -> putStrLn "Invalid entry"
---
+++ README.md
@@ -0,0 +1,9 @@
+# advent-of-code-2021
+
+See the [challenges](https://adventofcode.com/2021)
+
+## Why?
+
+- Learn Haskell
+- Challenge myself with pure FP :muscle:
+- Have fun solving problems
