58874557fa5b3863a32cdd73781f638ee6bc9d8f
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-09 22:56:33 -0500

    cp: argh!!

--- Day9.hs
+++ Day9.hs
@@ -10,33 +10,40 @@ day9Input = do
 testInput = 
   [
     "2199943210",
-    "3987894921",
-    "9856789892",
-    "8767896789",
-    "9899965678"
+    "3987894921"
+    -- "9856789892",
+    -- "8767896789",
+    -- "9899965678"
   ]
 
 
 mapFromArray line = Map.fromList (zip [0..length line] (map digitToInt line))
 
-leftRightNeighbors arrayMap =  map ( \(k,v) -> (v, Map.findWithDefault (-1) (k -1 ) arrayMap, Map.findWithDefault (-1) (k +1 ) arrayMap ) ) (Map.toList (arrayMap))
-topBottomNeighbors topArrayMap bottomArrayMap =  map ( \(k,v) -> (Map.findWithDefault (-1) k topArrayMap, Map.findWithDefault (-1) k bottomArrayMap ) ) (Map.toList (bottomArrayMap))
+leftNeighbors arrayMap =  Map.fromList (map ( \(k,v) -> (k, Map.findWithDefault (-1) (k -1) arrayMap) ) (Map.toList (arrayMap)))
+rightNeighbors arrayMap =  Map.fromList (map ( \(k,v) -> (k, Map.findWithDefault (-1) (k +1) arrayMap) ) (Map.toList (arrayMap)))
+topNeighbors topArrayMap =  Map.fromList (map ( \(k,v) -> (k, Map.findWithDefault (-1) (k) topArrayMap) ) (Map.toList (topArrayMap)))
+bottomNeighbors bottomArrayMap =  Map.fromList (map ( \(k,v) -> (k, Map.findWithDefault (-1) (k) bottomArrayMap) ) (Map.toList (bottomArrayMap)))
+-- topNeighbors topArrayMap =  map ( \(k,v) -> (v,Map.findWithDefault (-1) k topArrayMap )) (Map.toList (topArrayMap))
+-- bottomNeighbors bottomArrayMap =  map ( \(k,v) -> (v,Map.findWithDefault (-1) k bottomArrayMap ) ) (Map.toList (bottomArrayMap))
 
-getNeighbors lineAbove line lineBelow = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip (lrNeighbors line) (tbNeighbors lineAbove lineBelow)) where
-  lrNeighbors line = leftRightNeighbors (mapFromArray line)
-  tbNeighbors lineAbove lineBelow = topBottomNeighbors (mapFromArray (lineAbove)) (mapFromArray lineBelow)
+-- map (\entries -> filter (>=0) entries ) 
 
-iterateForNeighbors _ [] = []
-iterateForNeighbors prev [head] = getNeighbors prev head []
-iterateForNeighbors prev (head:neck:tail) = 
-  (getNeighbors prev head neck) ++ iterateForNeighbors head (neck:tail)
+-- argh the zip is the problem
+getNeighbors lineAbove line lineBelow = (mapFromArray line, lNeighbors line, rNeighbors line, tNeighbors lineAbove, bNeighbors lineBelow) where
+  lNeighbors line = leftNeighbors (mapFromArray line)
+  rNeighbors line = rightNeighbors (mapFromArray line)
+  tNeighbors lineAbove = topNeighbors (mapFromArray lineAbove)
+  bNeighbors lineBelow = bottomNeighbors (mapFromArray lineBelow)
 
-lowPoints :: [[Int]] -> [[Int]]
-lowPoints = filter ( (\(value:neighbors) -> value < (minimum neighbors)))
+-- iterateForNeighbors [] [head] = getNeighbors [] head []
+-- iterateForNeighbors prev [head, neck] = getNeighbors prev head neck ++ getNeighbors head neck []
+-- iterateForNeighbors prev (head:neck:tail) = 
+--   (getNeighbors prev head neck) ++ iterateForNeighbors head (neck:tail)
+
+-- lowPoints :: [[Int]] -> [[Int]]
+-- lowPoints = filter ( (\(value:neighbors) -> value < (minimum neighbors)))
 
   -- let 
-  --   lrNeighbors line = leftRightNeighbors (mapFromArray line)
-  --   tbNeighbors lineAbove lineBelow = topBottomNeighbors (mapFromArray (lineAbove)) (mapFromArray lineBelow)
   --   allneighborsTransformed = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip lrNeighbors tbNeighbors)
   --   lowPoints = map head (filter ( (\(value:neighbors) -> value < (minimum neighbors)))  allneighborsTransformed)
   -- in
