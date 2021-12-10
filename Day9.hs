ace3cc956a1ae68a4e5726f5584fc68c75753387
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-09 21:11:05 -0500

    cp2: iterate lines

--- Day9.hs
+++ Day9.hs
@@ -22,13 +22,21 @@ mapFromArray line = Map.fromList (zip [0..length line] (map digitToInt line))
 leftRightNeighbors arrayMap =  map ( \(k,v) -> (v, Map.findWithDefault (-1) (k -1 ) arrayMap, Map.findWithDefault (-1) (k +1 ) arrayMap ) ) (Map.toList (arrayMap))
 topBottomNeighbors topArrayMap bottomArrayMap =  map ( \(k,v) -> (Map.findWithDefault (-1) k topArrayMap, Map.findWithDefault (-1) k bottomArrayMap ) ) (Map.toList (bottomArrayMap))
 
-testMain =
-  let 
-    lrNeighbors = leftRightNeighbors (mapFromArray (testInput !! 0))
-    -- tbNeighbors = topBottomNeighbors (mapFromArray (testInput !! 0)) (mapFromArray (testInput !! 2))
-    tbNeighbors = topBottomNeighbors (mapFromArray ([])) (mapFromArray (testInput !! 1))
-    allneighborsTransformed = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip lrNeighbors tbNeighbors)
-    lowPoints = map head (filter ( (\(value:neighbors) -> value < (minimum neighbors)))  allneighborsTransformed)
-    -- traverseLines (head:neck:tail) = map ()
-  in
-    lowPoints 
+getNeighbors lineAbove line lineBelow = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip (lrNeighbors line) (tbNeighbors lineAbove lineBelow)) where
+  lrNeighbors line = leftRightNeighbors (mapFromArray line)
+  tbNeighbors lineAbove lineBelow = topBottomNeighbors (mapFromArray (lineAbove)) (mapFromArray lineBelow)
+
+testMain [] = []
+testMain [head] = getNeighbors [] head []
+testMain [head,neck] = getNeighbors head neck []
+testMain (head:neck:torso:tail) = 
+  (getNeighbors head neck torso) ++ testMain tail
+
+
+  -- let 
+  --   lrNeighbors line = leftRightNeighbors (mapFromArray line)
+  --   tbNeighbors lineAbove lineBelow = topBottomNeighbors (mapFromArray (lineAbove)) (mapFromArray lineBelow)
+  --   allneighborsTransformed = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip lrNeighbors tbNeighbors)
+  --   -- lowPoints = map head (filter ( (\(value:neighbors) -> value < (minimum neighbors)))  allneighborsTransformed)
+  -- in
+  --   lowPoints 
