7455a93a3dc72361c2cda6cdd763c2e8edc5c646
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-09 20:56:40 -0500

    progress: first line low points

--- Day9.hs
+++ Day9.hs
@@ -7,9 +7,28 @@ day9Input = do
   return (lines inputs)
 
 
-testInput = ["2199943210","3987894921","9856789892","8767896789","9899965678"]
+testInput = 
+  [
+    "2199943210",
+    "3987894921",
+    "9856789892",
+    "8767896789",
+    "9899965678"
+  ]
 
 
 mapFromArray line = Map.fromList (zip [0..length line] (map digitToInt line))
 
-leftRightNeighbors arrayMap =  map ( \(k,v) -> (Map.lookup (k -1 ) arrayMap, v, Map.lookup (k +1 ) arrayMap ) ) (Map.toList (arrayMap))
+leftRightNeighbors arrayMap =  map ( \(k,v) -> (v, Map.findWithDefault (-1) (k -1 ) arrayMap, Map.findWithDefault (-1) (k +1 ) arrayMap ) ) (Map.toList (arrayMap))
+topBottomNeighbors topArrayMap bottomArrayMap =  map ( \(k,v) -> (Map.findWithDefault (-1) k topArrayMap, Map.findWithDefault (-1) k bottomArrayMap ) ) (Map.toList (bottomArrayMap))
+
+testMain =
+  let 
+    lrNeighbors = leftRightNeighbors (mapFromArray (testInput !! 0))
+    -- tbNeighbors = topBottomNeighbors (mapFromArray (testInput !! 0)) (mapFromArray (testInput !! 2))
+    tbNeighbors = topBottomNeighbors (mapFromArray ([])) (mapFromArray (testInput !! 1))
+    allneighborsTransformed = map (\((v,ln,rn),(tn,bn)) -> filter (>=0) [v,ln,rn,tn,bn] ) (zip lrNeighbors tbNeighbors)
+    lowPoints = map head (filter ( (\(value:neighbors) -> value < (minimum neighbors)))  allneighborsTransformed)
+    -- traverseLines (head:neck:tail) = map ()
+  in
+    lowPoints 
