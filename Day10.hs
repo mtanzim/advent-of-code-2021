5ab79712d0d0803ea6d74f7504dab9542b5f0a29
Tanzim Mokammel <mtanzim@gmail.com>
2021-12-12 16:10:10 -0500

    filters

--- Day10.hs
+++ Day10.hs
@@ -30,8 +30,8 @@ data Expected = Expected Char deriving (Eq, Show)
 
 data Found = Found Char deriving (Eq, Show)
 
-traverseLine :: [Char] -> [Char] -> (Expected, Found)
-traverseLine _ [] = (Expected 'd', Found 'd')
+traverseLine :: [Char] -> [Char] -> Maybe (Expected, Found)
+traverseLine _ [] = Nothing
 traverseLine [] (curChar : rest) = traverseLine [curChar] rest
 traverseLine (topOfStack : restOfStack) (curChar : rest) =
   let isCloser = Map.member curChar closeToOpenMap
@@ -40,7 +40,7 @@ traverseLine (topOfStack : restOfStack) (curChar : rest) =
         then
           if curChar == expectedCloser
             then traverseLine restOfStack rest
-            else (Expected expectedCloser, Found curChar)
+            else Just (Expected expectedCloser, Found curChar)
         else traverseLine (curChar : topOfStack : restOfStack) rest
 
 testInput :: [String]
@@ -49,8 +49,20 @@ testInput =
     "[[<[([]))<([[{}[[()]]]",
     "[{[{({}]{}}([{[{{{}}([]",
     "[<(<(<(<{}))><([]([]()",
-    "<{([([[(<>()){}]>(<<{{"
+    "<{([([[(<>()){}]>(<<{{",
+    "[<>({}){}[([])<>]]"
   ]
 
-testMain :: [(Expected, Found)]
+calculateSyntaxScore :: [[Char]] -> [Maybe (Expected, Found)]
+calculateSyntaxScore = filter onlyCorrupted . map (traverseLine [])
+  where
+    onlyCorrupted Nothing = False
+    onlyCorrupted (Just _) = True
+
+main :: IO ()
+main = do
+  input <- day10Input
+  print (calculateSyntaxScore input)
+
+testMain :: [Maybe (Expected, Found)]
 testMain = map (traverseLine []) testInput
