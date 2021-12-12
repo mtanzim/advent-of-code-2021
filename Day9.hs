9374ea77f1cfea768b3325bb5467c8df58c4dda5
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-11 22:06:26 -0500

    clean up Day9

--- Day9.hs
+++ Day9.hs
@@ -1,94 +1,38 @@
 module Day9 where
 
 import Data.Char (digitToInt)
-import Data.List
+import Data.List (sort)
 import qualified Data.Map as Map
 
-day9Input :: IO [String]
+type Coordinate = (Int, Int)
+
+type CoordinateMap = Map.Map Coordinate Int
+
+type VisitedMap = Map.Map Coordinate Bool
+
+type RawInput = [[Char]]
+
+day9Input :: IO RawInput
 day9Input = do
   inputs <- readFile "day9Input.txt"
   return (lines inputs)
 
-type RawInput = [[Char]]
+main :: IO ()
+main = do
+  input <- day9Input
+  print (getSumOfRisk input)
+  print (getProductOfLargestBasins input)
 
-testInput :: RawInput
-testInput =
-  [ "2199943210",
-    "3987894921",
-    "9856789892",
-    "8767896789",
-    "9899965678"
-  ]
-
-mapFromArray :: [Char] -> Map.Map Int Int
-mapFromArray line = Map.fromList (zip [0 .. length line] (map digitToInt line))
-
-leftNeighbors :: (Ord k, Num a, Num k) => Map.Map k a -> Map.Map k a
-leftNeighbors arrayMap = Map.fromList (map (\(k, v) -> (k, Map.findWithDefault (-1) (k -1) arrayMap)) (Map.toList arrayMap))
-
-rightNeighbors arrayMap = Map.fromList (map (\(k, v) -> (k, Map.findWithDefault (-1) (k + 1) arrayMap)) (Map.toList arrayMap))
-
-topNeighbors topArrayMap = Map.fromList (map (\(k, v) -> (k, Map.findWithDefault (-1) k topArrayMap)) (Map.toList topArrayMap))
-
-bottomNeighbors bottomArrayMap = Map.fromList (map (\(k, v) -> (k, Map.findWithDefault (-1) k bottomArrayMap)) (Map.toList bottomArrayMap))
-
-getNeighbors :: [Char] -> [Char] -> [Char] -> [[Int]]
-getNeighbors lineAbove line lineBelow =
-  let valuesMap = mapFromArray line
-      lNeighbors = leftNeighbors (mapFromArray line)
-      rNeighbors = rightNeighbors (mapFromArray line)
-      tNeighbors = topNeighbors (mapFromArray lineAbove)
-      bNeighbors = bottomNeighbors (mapFromArray lineBelow)
-   in map
-        ( filter (>= 0)
-            . ( \idx ->
-                  [ Map.findWithDefault (-1) idx valuesMap,
-                    Map.findWithDefault (-1) idx lNeighbors,
-                    Map.findWithDefault (-1) idx rNeighbors,
-                    Map.findWithDefault (-1) idx tNeighbors,
-                    Map.findWithDefault (-1) idx bNeighbors
-                  ]
-              )
-        )
-        [0 .. (length line - 1)]
-
-iterateForNeighbors :: [Char] -> [[Char]] -> [[Int]]
-iterateForNeighbors [] [] = []
-iterateForNeighbors [] [head] = getNeighbors [] head []
-iterateForNeighbors prev [] = getNeighbors prev [] []
-iterateForNeighbors prev [head, neck] = getNeighbors prev head neck ++ getNeighbors head neck []
-iterateForNeighbors prev (head : neck : tail) =
-  getNeighbors prev head neck ++ iterateForNeighbors head (neck : tail)
-
-lowPoints :: [[Int]] -> [[Int]]
-lowPoints = filter (\(value : neighbors) -> value < minimum neighbors)
-
-testMain :: Int
-testMain = (sum . map ((+ 1) . head) . lowPoints . iterateForNeighbors []) testInput
-
-riskSum :: [[Char]] -> Int
-riskSum = sum . map ((+ 1) . head) . lowPoints . iterateForNeighbors []
-
--- for part B
-
-type Coordinate = (Int, Int)
-
-type CoordninateMap = Map.Map Coordinate Int
-
-type VisitedMap = Map.Map Coordinate Bool
-
-coordinateMap :: [String] -> Int -> Map.Map (Int, Int) Char -> Map.Map (Int, Int) Char
+coordinateMap :: RawInput -> Int -> Map.Map (Int, Int) Char -> Map.Map (Int, Int) Char
 coordinateMap [] _ curMap = curMap
-coordinateMap (curLine : tail) lineIdx curMap =
+coordinateMap (curLine : rest) lineIdx curMap =
   let mapFromLine = foldr (\charIdx acc -> Map.insert (lineIdx, charIdx) (curLine !! charIdx) acc) curMap [0 .. (length curLine - 1)]
-   in coordinateMap tail (lineIdx + 1) mapFromLine
+   in coordinateMap rest (lineIdx + 1) mapFromLine
 
-convertCoordinateMap :: Map.Map k Char -> Map.Map k Int
+convertCoordinateMap :: Map.Map Coordinate Char -> CoordinateMap
 convertCoordinateMap = Map.map digitToInt
 
-
-
-collectNeighbors :: Coordinate -> CoordninateMap -> CoordninateMap
+collectNeighbors :: Coordinate -> CoordinateMap -> CoordinateMap
 collectNeighbors (x, y) coordMap =
   Map.fromList $
     filter
@@ -115,13 +59,13 @@ findConnectedNeighbors (curCoord : rest) visitedMap coordMap =
       updatedVisited = foldr (\(curX, curY) curMap -> Map.insert (curX, curY) True curMap) visitedMap immediateNeighborCoords
    in findConnectedNeighbors updatedQueue updatedVisited coordMap
 
-sizeOfBasin :: CoordninateMap -> Coordinate -> Int
+sizeOfBasin :: CoordinateMap -> Coordinate -> Int
 sizeOfBasin coordMap startCoord =
   let connectedNeighbors = findConnectedNeighbors [startCoord] (Map.insert startCoord True Map.empty) coordMap
       valuesOfConnectedNeighbors = map (\(x, y) -> Map.lookup (x, y) coordMap) (Map.keys connectedNeighbors)
    in length valuesOfConnectedNeighbors
 
-collectLowPoints :: CoordninateMap -> [Coordinate]
+collectLowPoints :: CoordinateMap -> [Coordinate]
 collectLowPoints coordMap = filter fn (Map.keys coordMap)
   where
     fn coord =
@@ -130,7 +74,7 @@ collectLowPoints coordMap = filter fn (Map.keys coordMap)
           neighborVals = Map.elems neighbors
        in curVal < minimum neighborVals
 
-collectBasins :: [Coordinate] -> CoordninateMap -> [Int]
+collectBasins :: [Coordinate] -> CoordinateMap -> [Int]
 collectBasins toSearch coordMap = map (sizeOfBasin coordMap) toSearch
 
 getSumOfRisk :: RawInput -> Int
@@ -147,14 +91,13 @@ getProductOfLargestBasins rawInput =
       productOfLargestBasins = product . take 3 . reverse . sort
    in productOfLargestBasins (collectBasins lowPointCoords coordMap)
 
-main :: IO ()
-main = do
-  input <- day9Input
-  print (getSumOfRisk input)
-  print (riskSum input)
-  print (getProductOfLargestBasins input)
-
 -- DEBUG
 
-testMain' :: CoordninateMap -> [Int]
-testMain' = collectBasins [(0, 1), (0, 9), (2, 2), (4, 6)]
+testInput :: RawInput
+testInput =
+  [ "2199943210",
+    "3987894921",
+    "9856789892",
+    "8767896789",
+    "9899965678"
+  ]
