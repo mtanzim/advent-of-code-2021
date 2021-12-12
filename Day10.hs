3b38ad932cf495e5b56377d9c042ec9d84998952
Tanzim Mokammel <mtanzim@gmail.com>
2021-12-12 15:49:38 -0500

    think this works

--- Day10.hs
+++ Day10.hs
@@ -26,5 +26,21 @@ closeToOpenLst = map (\(open, close) -> (close, open)) openToCloseLst
 closeToOpenMap :: Map.Map Char Char
 closeToOpenMap = Map.fromList closeToOpenLst
 
+traverseLine :: [Char] -> [Char] -> [Char]
+traverseLine curStack [] = curStack
+traverseLine [] (curChar : rest) = traverseLine [curChar] rest
+traverseLine (topOfStack : restOfStack) (curChar : rest) =
+  let isCloser = Map.member curChar closeToOpenMap
+      expectedCloser = Map.findWithDefault 'e' topOfStack openToCloseMap
+   in if isCloser
+        then
+          if curChar == expectedCloser
+            then traverseLine restOfStack rest
+            else topOfStack : restOfStack
+        else traverseLine (curChar : topOfStack : restOfStack) rest
+
 testInput :: [Char]
 testInput = "{([(<{}[<>[]}>{[]{[(<()>"
+
+testMain :: [Char]
+testMain = traverseLine [] testInput
