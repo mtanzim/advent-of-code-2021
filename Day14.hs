030644f791e4f350fc46c3383e973acb966be70c
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-15 20:19:28 -0500

    .

--- Day14.hs
+++ Day14.hs
@@ -3,33 +3,33 @@ module Day14 where
 import Data.List.Split
 import qualified Data.Map as Map
 
--- testTemplate :: [Char]
--- testTemplate = "NNCB"
+testTemplate :: [Char]
+testTemplate = "NNCB"
 
--- type PairRule = (String, Char)
+type PairRule = (String, Char)
 
--- testPairRuleList :: [PairRule]
--- testPairRuleList =
---   [ ("CH", 'B'),
---     ("HH", 'N'),
---     ("CB", 'H'),
---     ("NH", 'C'),
---     ("HB", 'C'),
---     ("HC", 'B'),
---     ("HN", 'C'),
---     ("NN", 'C'),
---     ("BH", 'H'),
---     ("NC", 'B'),
---     ("NB", 'B'),
---     ("BN", 'B'),
---     ("BB", 'N'),
---     ("BC", 'B'),
---     ("CC", 'N'),
---     ("CN", 'C')
---   ]
+testPairRuleList :: [PairRule]
+testPairRuleList =
+  [ ("CH", 'B'),
+    ("HH", 'N'),
+    ("CB", 'H'),
+    ("NH", 'C'),
+    ("HB", 'C'),
+    ("HC", 'B'),
+    ("HN", 'C'),
+    ("NN", 'C'),
+    ("BH", 'H'),
+    ("NC", 'B'),
+    ("NB", 'B'),
+    ("BN", 'B'),
+    ("BB", 'N'),
+    ("BC", 'B'),
+    ("CC", 'N'),
+    ("CN", 'C')
+  ]
 
--- testPairRuleMap :: Map.Map String Char
--- testPairRuleMap = Map.fromList testPairRuleList
+testPairRuleMap :: Map.Map String Char
+testPairRuleMap = Map.fromList testPairRuleList
 
 makePairsOfTemplate :: [Char] -> [(Char, Char)]
 makePairsOfTemplate = reverse . go []
@@ -57,21 +57,20 @@ polymerizeOnce ruleMap input =
 polymerize :: Int -> Map.Map String Char -> [Char] -> [Char]
 polymerize n ruleMap start = foldr (\_ acc -> polymerizeOnce ruleMap acc) start [1 .. n]
 
-makeMapOfOccurence :: String -> Map.Map Char Int
-makeMapOfOccurence =
+makeMapOfOccurrence :: String -> Map.Map Char Int
+makeMapOfOccurrence =
   foldr (\curChar curMap -> Map.insert curChar (getExistingValue curChar curMap) curMap) Map.empty
   where
     getExistingValue curChar' curMap' = (+) 1 $ Map.findWithDefault 0 curChar' curMap'
 
--- testMain :: Int
--- testMain =
---   let occurenceMap = makeMapOfOccurence (polymerize 10 testPairRuleMap testTemplate)
---       values = Map.elems occurenceMap
---       minOcc = minimum values
---       maxOcc = maximum values
---    in maxOcc - minOcc
+testMain :: Int
+testMain =
+  let occurrenceMap = makeMapOfOccurrence (polymerize 10 testPairRuleMap testTemplate)
+      values = Map.elems occurrenceMap
+      minOcc = minimum values
+      maxOcc = maximum values
+   in maxOcc - minOcc
 
--- day14Input :: IO [String]
 day14Input :: IO (String, [([Char], Char)])
 day14Input = do
   inputs <- readFile "day14Input.txt"
@@ -90,8 +89,8 @@ main = do
 -- mainFn :: String -> Map.Map String Char -> Int
 mainFn :: [Char] -> Map.Map String Char -> Int
 mainFn template ruleMap =
-  let occurenceMap = makeMapOfOccurence (polymerize 40 ruleMap template)
-      values = Map.elems occurenceMap
+  let occurrenceMap = makeMapOfOccurrence (polymerize 20 ruleMap template)
+      values = Map.elems occurrenceMap
       minOcc = minimum values
       maxOcc = maximum values
    in maxOcc - minOcc
