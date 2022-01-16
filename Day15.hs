e7e164890aad971bc808f47defee6e68fe90fde0
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-15 20:58:43 -0500

    clean up

--- Day15.hs
+++ Day15.hs
@@ -16,13 +16,19 @@ day15Input = do
   inputs <- readFile "day15Input.txt"
   return (lines inputs)
 
-
-djikstra = do
+main :: IO ()
+main = do
     rawInput <- day15Input
+
+    let coordMap = convertCoordinateMap (coordinateMap rawInput 0 Map.empty)
+    print (findLeastRiskDjikstra coordMap (0,0))
+
+
+-- based on: https://algs4.cs.princeton.edu/44sp/DijkstraSP.java.html
+findLeastRiskDjikstra :: Map.Map Coordinate Int -> Coordinate -> Int
+findLeastRiskDjikstra coordMap source = do
     let 
-        source :: Coordinate
-        source = (0,0)
-        coordMap = convertCoordinateMap (coordinateMap rawInput 0 Map.empty)
+        -- initialize
         edgeTo :: Map.Map Coordinate Coordinate
         edgeTo = Map.empty
         distTo :: Map.Map Coordinate Int
@@ -32,13 +38,18 @@ djikstra = do
         visited :: Map.Map Coordinate Bool
         visited = foldr fnVisit Map.empty (Map.toList coordMap) where
             fnVisit (curCoord, _) acc = Map.insert curCoord False acc
-
         pq :: PQMin.MinQueue Vertex
         pq = PQMin.singleton (Vertex source 0)
-        rv = go pq edgeTo distTo visited where
+
+        -- iterate over priority queue
+        -- TODO: edgeTo tracking for the actual path
+        -- wait, the questions do not require it?
+        in go pq edgeTo distTo visited where
             go q et dt vst = 
                 if PQMin.null q then
-                    dt
+                    -- findMax finds the bottom right point
+                    -- the value of this is the shortest weighted distance from source
+                    snd $ Map.findMax dt
                 else
                     let 
                         Vertex fromCoord _ = PQMin.findMin q
@@ -47,6 +58,7 @@ djikstra = do
                         neighbors = collectNeighbors fromCoord coordMap
                         filteredNeighborsLst = Map.toList $ Map.filterWithKey (\k _ -> not $ Map.findWithDefault False k vst') neighbors
                         zero = (PQMin.deleteMin q, et, dt)
+                        -- iterate over neighbors
                         (uq, uet, udt) = foldr relax zero filteredNeighborsLst where
                             relax (toCoord, weight) (q', et', dt') =
                                 let
@@ -54,21 +66,13 @@ djikstra = do
                                     toDistTo = Map.findWithDefault 0 toCoord dt'
                                     minDistTo = min toDistTo (fromDistTo + weight)
                                     updatedDistTo = Map.insert toCoord minDistTo dt'
-                                    -- updatedDistTo = Map.mapWithKey udtf dt' where
-                                    --     udtf k v = if k == toCoord then minDistTo else v
-
                                     -- curEdgeTo = Map.findWithDefault toCoord fromCoord et'
                                     -- updatedCurEdgeTo = if minDistTo < toDistTo then toCoord else curEdgeTo
                                     -- updatedEdgeTo = Map.mapWithKey uetf et' where
                                     --     uetf k v = if k == fromCoord then updatedCurEdgeTo else v
-
                                     filteredQ = PQMin.filter (\(Vertex coord' _) -> toCoord /= coord') q'
                                     updatedQ = PQMin.insert (Vertex toCoord minDistTo) filteredQ
-
-
                                 in
                                     (updatedQ, Map.empty, updatedDistTo)
                         in go uq uet udt vst'
-    -- print (rv)
-    print (Map.findMax rv)
     
