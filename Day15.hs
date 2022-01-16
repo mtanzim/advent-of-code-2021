cf82e3f34cea68d2160ff89192c063c7bbc43850
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-15 20:40:22 -0500

    clean up

--- Day15.hs
+++ Day15.hs
@@ -6,20 +6,6 @@ import qualified Data.Map as Map
 import qualified Data.PQueue.Min as PQMin
 import Data.Maybe
 
-
-data TrackerInfo = TrackerInfo {
-    distTo :: Int,
-    edgeTo :: EdgeTo
-} deriving (Eq, Show)
-
-data EdgeTo = EdgeTo {
-    from :: Coordinate,
-    to:: Coordinate
-} deriving (Eq, Show)
-
-
-type Tracker = Map.Map Coordinate TrackerInfo
-
 data Vertex = Vertex Coordinate Int deriving (Eq, Show)
 
 instance Ord Vertex where
@@ -82,47 +68,6 @@ djikstra = do
                                 in
                                     (updatedQ, Map.empty, updatedDistTo)
                         in go uq uet udt vst'
-
-
-
-
-
-
-        -- go pq distTo
-    -- print (PQMin.toList pq)
-    -- print (distTo)
     print (rv)
+    print (Map.findMax rv)
     
-
-
-
-
-testMain :: Coordinate -> IO ()
-testMain coord = do
-    rawInput <- day15Input
-    let
-        coordMap = convertCoordinateMap (coordinateMap rawInput 0 Map.empty)
-        neighbors :: CoordinateMap
-        neighbors = collectNeighbors coord coordMap
-        neighborsLst = Map.toList neighbors
-        closestNeighbor :: (Coordinate, Int)
-        closestNeighbor =  foldr findMin (head neighborsLst) neighborsLst where 
-            findMin (curCoord, risk) (minCoord, minRisk) = 
-                if risk < minRisk 
-                    then (curCoord, risk) 
-                    else (minCoord, minRisk)
-        updatedTracker :: Tracker
-        updatedTracker = foldr fn Map.empty neighborsLst where
-            fn (curCoord, risk) acc = 
-                Map.insert 
-                    curCoord
-                    TrackerInfo {
-                        distTo=risk, 
-                        edgeTo=
-                            EdgeTo {from=coord,to=curCoord}
-                    } 
-                    acc
-     
-    print (neighbors)
-    print (closestNeighbor)
-    print (updatedTracker)
